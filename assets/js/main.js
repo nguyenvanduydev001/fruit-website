c0b88b74184dbdd83cc1f61768d6b921e6960083
nguyenvanduydev001 <nguyenvanduydev001@gmail.com>
2024-07-31 20:08:30 +0700

    fourth commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,3 +82,16 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true,
+})
+
+sr.reveal(`.home__data, .join__container, .footer`)
+sr.reveal(`.home__img`, { origin: 'bottom' })
+sr.reveal(`.enjoy__card, .popular__card`, { interval: '100' })
+sr.reveal(`.about__data`, { origin: 'right' })
+sr.reveal(`.about__img`, { origin: 'left' })
